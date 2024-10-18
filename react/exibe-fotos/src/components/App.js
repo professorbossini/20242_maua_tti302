48edce361ea1bc1a511b64ae5e1c7aefe28365ff
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-18 11:02:40 -0300

    ap07(react): encerra secao 2.8
    - fizemos o react-dotenv
    - retomar instalando o sdk da pexels na secao 2.9

--- react/exibe-fotos/src/components/App.js
+++ react/exibe-fotos/src/components/App.js
@@ -1,17 +1,27 @@
+import React from 'react'
 import Busca from "./Busca"
+import env from 'react-dotenv'
 
-export default () => {
-  return (
-    <div className='grid m-auto w-9 border-round border-1 border-400'>
-      {/* // div>h1{Exibir uma lista de...} */}
-      <div className='col-12'>
-        <div>
-          <h1>Exibir uma lista de...</h1>
+export default class App extends React.Component {
+
+  onBuscaRealizada = (termo) => {
+    console.log(termo)
+  }
+  render(){
+    console.log(env.PEXELS_KEY)
+    console.log(window.env.PEXELS_KEY)
+    return (
+      <div className='grid m-auto w-9 border-round border-1 border-400'>
+        {/* // div>h1{Exibir uma lista de...} */}
+        <div className='col-12'>
+          <div>
+            <h1>Exibir uma lista de...</h1>
+          </div>
+        </div>
+        <div className='col-12'>
+          <Busca onBuscaRealizada={this.onBuscaRealizada}/>
         </div>
       </div>
-      <div className='col-12'>
-        <Busca />
-      </div>
-    </div>
-  )
+    )
+  }
 }
--- react/exibe-fotos/src/components/Busca.js
+++ react/exibe-fotos/src/components/Busca.js
@@ -9,17 +9,42 @@ export default class Busca extends React.Component{
     termoDeBusca: ''
   }
 
+  // exercício
+  //fazer com que o cmoponente textual exiba sempre letras maiúsculas
+  //feito em aula
+
+  onTermoAlterado = (event) => {
+    // console.log(event.target.value)
+    //não faça isso, caso contrário a tela não vai atualizar
+    // this.state.termoDeBusca = event.target.value
+    this.setState({termoDeBusca: event.target.value})
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
+  }
   
   render(){
     return (
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column'>
+          {/* InputIcon>(InputIcon+InputText) */}
+          <IconField iconPosition="left">
+            <InputIcon className="pi pi-search"> </InputIcon>
+            <InputText 
+              placeholder={this.props.dica}
+              className='w-full'
+              onChange={this.onTermoAlterado}
+              value={this.state.termoDeBusca}/>
+          </IconField>
+          <Button 
+            label='OK'
+            className='p-button-outlined mt-2'
+          />
+        </div>
+      </form>
       // div.flex.flex-column
-      <div className='flex flex-column'>
-        {/* InputIcon>(InputIcon+InputText) */}
-        {/* <IconField iconPosition="left">
-          <InputIcon className="pi pi-search"> </InputIcon>
-          <InputText placeholder={this.props.dica} />
-        </IconField> */}
-      </div>
     )
   }
 }
