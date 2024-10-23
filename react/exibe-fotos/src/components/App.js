f292cf4ae6d19f3e01ce733bedf25d8f2cfa40ff
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-23 09:03:53 -0300

    ap07(react): encerra a secao 2.10
    - retomar definindo componentes para lista de imagem e para imagem única

--- react/exibe-fotos/src/components/App.js
+++ react/exibe-fotos/src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import Busca from "./Busca"
 import env from 'react-dotenv'
-
+import { createClient } from 'pexels'
 export default class App extends React.Component {
 
-  onBuscaRealizada = (termo) => {
-    console.log(termo)
+  state = {
+    pics: []
+  }
+
+  pexelsClient = null
+  
+  //método de ciclo de vida
+  componentDidMount(){
+    this.pexelsClient = createClient (env.PEXELS_KEY)
+  }
+
+  onBuscaRealizada = (query) => {
+    this.pexelsClient.photos.search({query, per_page: 10}).then((pics) => {
+      this.setState({pics: pics.photos})
+    })     
   }
   render(){
     console.log(env.PEXELS_KEY)
@@ -20,6 +33,16 @@ export default class App extends React.Component {
         </div>
         <div className='col-12'>
           <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        <div className='col-12'>
+          {
+            //iterar sobre a coleçaõ de fotos, mostrando uma por uma, use a foto small e também o alt
+            this.state.pics.map((pic, key) => (
+              <div key={key}>
+                  <img src={pic.src.small} alt={pic.alt}/>
+              </div>
+            ))
+          }
         </div>
       </div>
     )
